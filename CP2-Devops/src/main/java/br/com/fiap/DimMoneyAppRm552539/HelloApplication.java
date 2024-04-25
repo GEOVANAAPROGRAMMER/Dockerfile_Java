dfda1f1c9495cd4c71fb8727d4cbc549bc576aec
Geovana Ribeiro D Silva <125618145+GEOVANAAPROGRAMMER@users.noreply.github.com>
2024-04-24 23:34:58 -0300

    Criação de imagem Java

---
+++ CP2-Devops/src/main/java/br/com/fiap/DimMoneyAppRm552539/HelloApplication.java
@@ -0,0 +1,9 @@
+package br.com.fiap.DimMoneyAppRm552539;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
